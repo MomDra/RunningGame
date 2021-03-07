832ac9e66a4b85bc3e89b16fee5bb9ddb4862a22
MomDra <yem9087@koreatech.ac.kr>
2021-03-08 00:11:55 +0900

    Physics and Ground

---
+++ Assets/Scripts/GroundManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundManager : MonoBehaviour
+{
+    [SerializeField]
+    GameObject Player;
+
+    [SerializeField]
+    float LastPos = -5;
+    [SerializeField]
+    int LastScale = 10;
+
+    [SerializeField]
+    int MaxScale = 8;
+    [SerializeField]
+    int MaxYPos = 4;
+
+    [SerializeField]
+    Sprite GroundSprite;
+
+
+    private void Update()
+    {
+        MakeGround();
+    }
+
+    void MakeGround()
+    {
+        // 새로운 바닥을 생성
+        if(Player.transform.position.x + 20 >= LastPos + LastScale * 0.5f)
+        {
+            int newScale = Random.Range(3, MaxScale);
+            float newX = LastPos + LastScale * 0.5f + newScale * 0.5f + Random.Range(3, 5);
+            GameObject ground = new GameObject("Ground");
+            ground.AddComponent<BoxCollider2D>();
+            ground.AddComponent<SpriteRenderer>();
+            SpriteRenderer sr = ground.GetComponent<SpriteRenderer>();
+            sr.sprite = GroundSprite;
+
+            ground.transform.position = new Vector3(newX, Random.Range(-MaxYPos, MaxYPos), 0);
+            ground.transform.localScale = new Vector3(newScale, 1, 1);
+
+            LastPos = newX;
+            LastScale = newScale;
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    int MoveSpeed = 5;
+
+    [SerializeField]
+    int JumpPower = 500;
+
+    [SerializeField]
+    int JumpCount = 2;
+
+    int gamejumpcount;
+
+    Rigidbody2D Myrigid;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        Myrigid = GetComponent<Rigidbody2D>();
+        gamejumpcount = JumpCount;
+    }
+
+    private void Start()
+    {
+        Myrigid.AddForce(new Vector2(0, JumpPower));
+    }
+
+    void Update()
+    {
+        transform.position += Vector3.right * MoveSpeed * Time.deltaTime;
+
+
+        if (Input.GetKeyDown(KeyCode.Space) && gamejumpcount > 0)
+        {
+            Myrigid.AddForce(new Vector2(0, JumpPower));
+            gamejumpcount--;
+        }
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        gamejumpcount = JumpCount;
+    }
+}
